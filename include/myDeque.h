c9ddf552968865908c31ff5c1c3045a4b8e14065
bnatalha <natalia_eletro@hotmail.com>
2017-06-20 03:52:48 -0300

    myDeque pronto. começando testes

--- include/myDeque.h
+++ include/myDeque.h
@@ -17,7 +17,7 @@
 
 	/**
 	* @class myDeque
-	* @brief Um deque que usa um array circular alocado dinâmicamente
+	* @brief Um deque que usa um array circular alocado dinâmicamente (sem iterator)
 	* @tparam T Tipo dos elementos da lista
 	*/
 	template < typename T >
@@ -27,92 +27,16 @@
 			int capacidade = 50;	/**< Capacidade máxima do deque */
 			int ini;	/**< Índice para o inicio do deque */
 			int fim;	/**< Índice para o fim do deque */
-			T* fila;	/**< Onde os elementos do deque serão guardados*/
+			T* recipiente;	/**< Onde os elementos do deque serão guardados*/
+
+			void my_reallocator();
 
 		public:
-			/**
-			* @class const_iterator
-			* @brief iterator constante de myDeque
-			* @sa const no fim da assinatura (https://stackoverflow.com/questions/1549869/)
-			*/
-			class const_iterator
-			{
-				public:
-					// Construtor
-					const_iterator();
-
-					// Acesso
-					const T& operator*() const;
-
-					// Movimentação
-					const_iterator& operator++ ();	// pré-incrementação
-					const_iterator operator++ (int);	// pós-incrementação
-					const_iterator& operator-- ();	// pré-decrementação
-					const_iterator operator-- (int);	// pós-decrementação
-
-					// Comparação
-					bool operator==( const const_iterator & it_direita ) const;
-					bool operator!=( const const_iterator & it_direita ) const;
-
-				protected:
-					// Construtor
-					const_iterator( T *ponteiro )
-						: atual (ponteiro)
-					{} 
-
-					// Atributos
-					T * atual;	/**< Ponteiro para um elemento do deque */
-
-					// Permissão
-					friend class myDeque<T>;
-			};
-
-			/**
-			* @class iterator
-			* @brief iterator "modificável" de myDeque
-			* @sa const no fim da assinatura (https://stackoverflow.com/questions/1549869/)
-			*/
-			class iterator : public const_iterator
-			{
-				public:
-					// Construtor
-					iterator()
-						: const_iterator()
-					{}
-
-					// Acesso ao conteudo do iterator
-					const T& operator*() const;
-					T& operator*();
-
-					// Movimentação
-					iterator& operator++ ();	// pré-incrementação
-					iterator operator++ (int);	// pós-incrementação
-					iterator& operator-- ();	// pré-decrementação
-					iterator operator-- (int);	// pós-decrementação
-						
-					// Atribuição
-					iterator & operator= ( const iterator& it_direita );
-
-				protected:
-					// Construtor
-					iterator ( T *ponteiro)
-						: const_iterator(ponteiro)
-					{}
-
-					// Permissão
-					friend class myDeque<T>;	
-			};
-
 			// Construtor
 			myDeque();
 			myDeque( const myDeque & orig );
 			// Destrutor
 			~myDeque();
-			// Iterator
-			const_iterator begin() const;
-			iterator begin();
-			const_iterator end() const;
-			iterator end();
 			// Acesso a elementos
 			T& operator[] (int n);
 			T& front();
@@ -121,24 +45,14 @@
 			int size();
 			bool empty();
 			// Modificadores
-			//void push_sorted(const T& elem );
 			void push_sorted( T elem );
-			void push_back( const T& elem );	// bool se tiver como checar se pode ser alocado ou não
+			void push_back( const T& elem );
 			bool pop_back();
 			void push_front( const T& elem );
 			bool pop_front();
 			void clear();
-			void erase( iterator &it );
-			//insert()	iterator
-			// Operações
-			void remove(const T& val);
-			void unique();
-			// > Auxiliares dos modificadores
-			bool exist(const T& elem);
-			//sort();
 			// Sobrecarga de operadores
 			bool operator== ( myDeque<T> d_direita);
-			bool operator!= ( myDeque<T> d_direita);
 			myDeque<T>& operator= (const myDeque<T>& copy);
 			// PARA TESTE: sobrecarga operador <<
 			//template <typename foo>
@@ -147,6 +61,36 @@
 
 namespace edb1
 {
+	/**
+	* @brief Realoca o container do deque para o dobro de sua capacidade total
+	*/
+	template <typename T>
+	void myDeque<T>::my_reallocator()
+	{
+		cout << "(dobrando capacidade)";
+
+		// tenta alocar o dobro da capacidade do deque atual
+		try{
+			T* new_recipiente = new T[capacidade*2];	// cria um novo recipiente com o dobro de capacidade
+		}catch (std::bad_alloc& ba)		{
+			cout << << "bad_alloc caught: " << ba.what() << endl;
+		}
+		
+		for (int n = 0, i = ini; n < size(); n++)	// atribui ao novo bloco de memoria informações do atual
+		{
+			new_recipiente[n] = recipiente[i];
+			i = ((i+1) % capacidade);
+		}
+
+		// fixa indices 
+		ini = ( empty()? -1 : 0 );
+		fim = size() -1;
+		
+
+		delete[] recipiente;	// Deleta bloco de memoria apontado por 'recipiente'
+		recipiente = new_recipiente;	// aponta 'recipiente' para o bloco de memoria criado
+	}
+
 	// =================================================================================================
 	// =========================================== Construtores ========================================
 	// =================================================================================================
@@ -155,15 +99,13 @@ namespace edb1
 	* @brief Constrói um objeto myDeque vazio
 	*/
 	template <typename T>
-	myDeque<T>::myDeque()	: ini(-1), fim(-1)
+	myDeque<T>::myDeque()	: capacidade(4), ini(-1), fim(-1)
 	{
 		// tenta alocar o Deque.
 		try{
-			fila = new T[capacidade];
-		}		
-		catch( exception &e)
-		{
-			cerr << e.what() << endl;
+			recipiente = new T[capacidade];
+		}catch (std::bad_alloc& ba)		{
+			cout << << "bad_alloc caught: " << ba.what() << endl;
 		}
 
 	}
@@ -173,44 +115,36 @@ namespace edb1
 	* @param objeto myDeque que será usado como referência para ser copiado
 	*/
 	template <typename T>
-	myDeque<T>::myDeque( const myDeque & orig )	: ini(orig.ini), fim(orig.fim)
+	myDeque<T>::myDeque( const myDeque & orig )	: capacidade(orig.capacidade), ini(orig.ini), fim(orig.fim)
 	{
 		// tenta alocar o Deque com a capacidade do deque 'orig'.
 		try{
-			fila = new T[orig.capacidade];
-		}		
-		catch( exception &e)
-		{
-			cerr << e.what() << endl;
+			recipiente = new T[orig.capacidade];		
+		}catch (std::bad_alloc& ba)		{
+			cout << << "bad_alloc caught: " << ba.what() << endl;
 		}
 
 		// copia os elementos de orig para este deque
-		for(i = 0; i < orig.size(); i++) this->fila[i] = orig.fila[i];
+		for(i = 0; i < orig.size(); i++) this->recipiente[i] = orig.recipiente[i];
 	}
 
 	// =========================================== Destrutor ========================================
 	
 	/**
-	* @brief Destrói um objeto myDeque, destruindo sua 'fila'
+	* @brief Destrói um objeto myDeque, destruindo sua 'recipiente'
 	*/
 	template <typename T>
 	myDeque<T>::myDeque()
 	{
-		delete[] fila;
+		delete[] recipiente;
 	}
-
-	// =================================================================================================
-	// =========================================== Iterator ============================================
-	// =================================================================================================
-
-	//
 
 	// =================================================================================================
 	// =========================================== Acesso ==============================================
 	// =================================================================================================
 
 	/**
-	* @return Referência para o elemento na posição 'n' do container 'fila'
+	* @return Referência para o elemento na posição 'n' do container 'recipiente'
 	*/
 	template <typename T>
 	T& myDeque<T>::operator[] (int n)
@@ -218,7 +152,7 @@ namespace edb1
 		if(empty())	// Se deque estiver vazio
 			throw std::out_of_range ("[EXCEPTION] operator[]: Não há elementos no deque");
 
-		return fila[ (ini + n )% capacidade ];
+		return recipiente[ (ini + n )% capacidade ];
 	}
 
 	/**
@@ -234,11 +168,11 @@ namespace edb1
 		if(n => size)	// Se a posição requisitada for maior do que o tamanho do deque
 			throw std::out_of_range ("[EXCEPTION] operator[]: posição requisitada > tamanho do deque");
 
-		return fila[ (ini + n )% capacidade ];
+		return recipiente[ (ini + n )% capacidade ];
 	}
 
 	/**
-	* @return Referência para o elemento no início da fila, se existir.
+	* @return Referência para o elemento no início da recipiente, se existir.
 	*/
 	template <typename T>
 	T& myDeque<T>::front()
@@ -246,20 +180,140 @@ namespace edb1
 		if(empty())	// Se deque estiver vazio, chamar exceção
 			throw std::out_of_range ("[EXCEPTION] front(): Não há elementos no deque");
 
-		return fila[ini];
+		return recipiente[ini];
 	}
 
 	/**
-	* @return Referência para o elemento no fim da fila,  se existir.
+	* @return Referência para o elemento no fim da recipiente,  se existir.
 	*/
 	template <typename T >
 	T& myFila<T>::back()
 	{
 		if (empty())	// Se deque estiver vazio, chamar exceção
-			throw std::out_of_range ("[EXCEPTION] back(): Não há elementos na fila");
+			throw std::out_of_range ("[EXCEPTION] back(): Não há elementos na recipiente");
 		
-		return fila[fim];
+		return recipiente[fim];
 	}
+
+	// =================================================================================================
+	// ======================================== Capacidade =============================================
+	// =================================================================================================
+
+
+	/**
+	* @retval true A deque está vazio
+	* @retval false A deque \b não está vazio
+	*/
+	template <typename T >
+	bool myDeque<T>::empty()
+	{
+		return (size() == 0);
+	}
+
+	/**
+	* @return Referência para o tamanho da deque
+	*/
+	template <typename T >
+	int myDeque<T>::size()
+	{
+		if (ini == -1) return 0;
+		else if(ini == fim ) return 1;
+		else if(ini < fim) return ((fim-ini)+1);
+		//else if (ini > fim)
+		return ( (fim + (capacidade - ini) )+ 1);
+	}
+
+	// =================================================================================================
+	// ======================================= Modificadores ===========================================
+	// =================================================================================================
+
+	/**
+	* @brief Acrescenta um elemento no final do Deque
+	* @param element Elemento do tipo T a ser acrescentado na frente da fila
+	*/
+	template <typename T>
+	void myDeque<T>::push_back( const T& elem )
+	{	
+		if(size() == capacidade)	
+			my_reallocator();	// Realocar o tamanho do container do Deque quando atingir a capacidade maxima inicial
+		
+		if (empty())	// se for o primeiro elemento a entrar na 'recipiente'
+			ini++;	// coloca 'ini' para apontar
+
+		fim = (fim+1)%capacidade;	// avança indice 'fim'
+		recipiente[fim] = element;	// Acrescenta elemento e depois aumenta o tamanho da 'recipiente'
+	}
+	/**
+	* @brief Remove o elemento no fim do Deque
+	*/
+	template <typename T>
+	void myDeque<T>::pop_back()
+	{
+		if (empty())
+			throw std::length_error ("[EXCEPTION] pop_back(): Não pode remover elementos de um deque vazio");
+		
+
+		recipiente[fim].~T();	// destrói elemento do fim
+
+		if (size() == 1)	// se só existir um elemento no 'recipiente' antes do pop()
+		{
+			ini = -1;	// fixa índices para fora da 'recipiente'
+			fim = -1;
+		}
+		else
+			fim = (fim-1)%capacidade;	// retrocede indice 'fim'
+		
+	}
+
+	/**
+	* @brief Acrescenta um elemento no início do Deque
+	* @param element Elemento do tipo T a ser acrescentado na frente da 'recipiente'
+	*/
+	template <typename T>
+	void myDeque<T>::push_front( const T& elem );
+	{	
+		if(size() == capacidade)	
+			my_reallocator();	// Realocar o tamanho do container do Deque quando atingir a capacidade maxima inicial
+		
+		if (empty())	// se for o primeiro elemento a entrar na 'recipiente'
+			fim++;	// coloca 'fim' para apontar
+
+		ini = (ini-1)%capacidade;	// retrocede indice 'ini'
+		recipiente[ini] = element;	// Acrescenta elemento e depois aumenta o tamanho da 'recipiente'
+	}
+
+	/**
+	* @brief Remove o elemento no início do Deque
+	*/
+	template <typename T>
+	void myDeque<T>::pop_front();
+	{
+		if (empty())
+			throw std::length_error ("[EXCEPTION] pop_front(): Não pode remover elementos de um deque vazio");
+		
+
+		recipiente[ini].~T();	// destrói elemento do ini
+
+		if (size() == 1)	// se só existir um elemento no 'recipiente' antes do pop()
+		{
+			ini = -1;	// fixa índices para fora da 'recipiente'
+			fim = -1;
+		}
+		else
+			ini = (ini+1)%capacidade;	// avança indice 'ini'
+		
+	}
+
+	/**
+	* @brief Remove todos os elementos do Deque
+	*/
+	template <typename T>
+	void myDeque<T>::clear()
+	{
+		while (empty() == false) pop_back();
+
+	}
+
 
 	// =================================================================================================
 	// ================================== Sobrecarga de operadores =====================================
@@ -281,7 +335,7 @@ namespace edb1
 			// checa cada elemento das listas, se estão na mesma ordem.
 			for(auto count = 0; count < size(); count++ )
 			{
-				if( fila[i] != d_direita.fila[dir_i] ) return false;	// achou elementos diferentes
+				if( recipiente[i] != d_direita.recipiente[dir_i] ) return false;	// achou elementos diferentes
 				
 				// incrementa indices
 				i = (i+1)%capacidade;
